020a81f3922ed271f1107008e83a3e98430749f1
Khomchenko Ivan <khomchenko_van@mail.ru>
2024-05-25 17:38:53 +0700

    new_commit

--- hw_1.sql
+++ hw_1.sql
@@ -1,38 +1,48 @@
 create table if not exists singer(
 id SERIAL primary key,
-name VARCHAR not null 
+name VARCHAR(30) not null 
 );
 
 create table if not exists song_list(
 id serial primary key,
-name VARCHAR not null,
+name VARCHAR(25) not null,
 issue_date DATE not null,
-id_genre INTEDER references genre(id)
+album_id INTEGER references album(id)
 );
 
 create table if not exists genre(
 id SERIAL primary key,
-name VARCHAR not null
+name VARCHAR(25) not null
 );
 
 create table if  not exists genre_singer(
 id_singer INTEGER references singer(id),
-id_genre INTEGER references genre(id)
+id_genre INTEGER references genre(id),
+primary key(id_singer, id_genre)
 );
 
 create table if  not exists album(
 id serial primary key,
-name VARCHAR not null,
-issue_date DATE not null,
-song_list INTEGER references song(id)
+name VARCHAR(30) not null,
+issue_date DATE not null
 );
 
 create table if  not exists album_singer(
 album_id INTEGER references album(id),
-singer_id INTEGER references singer(id)
+singer_id INTEGER references singer(id),
+primary key(album_id, singer_id)
 );
 
 create table if not exists collection(
 id serial primary key,
-song_col INTEGER references song_list(id)
+name VARCHAR(30) not null
 );
+
+create table if not exists collection_song_list(
+collection_id INTEGER references collection(id),
+song_id INTEGER references song_list(id),
+primary key(collection_id, song_id)
+);
+
+
+
